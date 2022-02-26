0f8e4a56f4c3e22b15150be4067494b82a68d689
half-ghost <55418764+half-ghost@users.noreply.github.com>
2022-02-26 15:16:31 +0800

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import requests
 # 配置项
 # 代理ip设置
 proxies={
-    'https':'http://127.0.0.1:33334'
+
 }
 
 # 图片形式的消息里最多展现的项目数量
@@ -19,7 +19,7 @@ send_pic_mes = False
 # 其他必需的配置项，不了解的话请勿乱改
 s = requests.session()
 s.keep_alive = False
-FILE_PATH = os.path.join(os.path.dirname(__file__))
+FILE_PATH = os.path.dirname(__file__)
 url_new = "https://store.steampowered.com/search/results/?l=schinese&query&sort_by=Released_DESC&category1=998&os=win&infinite=1&start=0&count=50"
 url_specials = "https://store.steampowered.com/search/results/?l=schinese&query&sort_by=_ASC&category1=998&specials=1&os=win&filter=topsellers&start=0&count=50"
 def other_request(url, headers = None):
