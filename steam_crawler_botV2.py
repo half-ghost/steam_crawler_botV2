be4759750682fa3072f800dfeb1cfa7d3bb28e72
half-ghost <55418764+half-ghost@users.noreply.github.com>
2023-03-14 01:02:25 +0800

    Update steam_crawler_botV2.py

--- steam_crawler_botV2.py
+++ steam_crawler_botV2.py
@@ -1,104 +1,110 @@
-from bs4 import BeautifulSoup as bs
-import re
-import json
-import os
-from .config import *
-from hoshino import Service
-from .take_my_money import pic_creater
-from PIL import Image
 import base64
 import io
+import json
+import os
+import re
+
+from bs4 import BeautifulSoup as bs
+from hoshino import Service
+from PIL import Image
+
+from .config import *
+from .take_my_money import pic_creater
 
 sv = Service("stbot-steam")
 
-with open(os.path.join(FILE_PATH, "data/tag.json"), "r", encoding="utf-8")as f:
+with open(os.path.join(FILE_PATH, "data/tag.json"), "r", encoding="utf-8") as f:
     tagdata = json.loads(f.read())
 # steam爬虫
-def steam_crawler(url_choose:str):
+def steam_crawler(url_choose: str):
     result = []
     get_url = other_request(url_choose).text
     soup = bs(get_url.replace(r"\n", "").replace(r"\t", "").replace(r"\r", "").replace("\\", ""), "lxml")
-    row_list = soup.find_all(name = "a", class_ = "search_result_row")
+    row_list = soup.find_all(name="a", class_="search_result_row")
     for row in row_list:
-        gameinfo = {}
-        gameinfo['标题'] = row.find(name = "span", class_ = "title").text
-        href = row.get("href")
-        gameinfo['链接'] = href
         appid = row.get("data-ds-appid")
-        gameinfo['appid'] = appid
-        gameinfo['高分辨率图片'] = "https://media.st.dl.pinyuncloud.com/steam/apps/" + appid + "/capsule_231x87.jpg"
-        gameinfo['低分辨率图片'] = row.find(name = "img").get("src")
-        if str(row.strike) == "None" :
+        gameinfo = {
+            "标题": row.find(name="span", class_="title").text,
+            "链接": row.get("href"),
+            "appid": appid,
+            "高分辨率图片": f"https://media.st.dl.pinyuncloud.com/steam/apps/{appid}/capsule_231x87.jpg",
+            "低分辨率图片": row.find(name="img").get("src"),
+        }
+        if str(row.strike) == "None":
             try:
-                price = row.find(name = "div", class_ = "col search_price responsive_secondrow").text.replace("\r", "").replace("\n", "").replace(" ", "")
-                gameinfo['折扣价'] = " "
-                if price!="" and "免费" not in price and "Free" not in price:
-                    gameinfo['原价'] = price
+                price = (
+                    row.find(name="div", class_="col search_price responsive_secondrow")
+                    .text.replace("\r", "")
+                    .replace("\n", "")
+                    .replace(" ", "")
+                )
+                gameinfo["折扣价"] = " "
+                if price != "" and "免费" not in price and "Free" not in price:
+                    gameinfo["原价"] = price
                 elif "免费" in price or "Free" in price:
-                    gameinfo['原价'] = "免费开玩"
+                    gameinfo["原价"] = "免费开玩"
                 else:
-                    gameinfo['原价'] = "无价格信息"
-            except:
-                gameinfo['原价'] = "无价格信息"
-                gameinfo['折扣价'] = " "
+                    gameinfo["原价"] = "无价格信息"
+            except Exception:
+                gameinfo["原价"] = "无价格信息"
+                gameinfo["折扣价"] = " "
         else:
             discount_price = re.findall(r"<br/>(.*?)  ", str(row))[0]
-            discount_percent = row.find(name = "div", class_ = "col search_discount responsive_secondrow").text.replace("\n", "").strip()
-            gameinfo['原价'] = row.strike.string.strip().replace(" ", "")
-            gameinfo['折扣价'] = f'{str(discount_price).strip().replace(" ", "")}({discount_percent})'
+            discount_percent = (
+                row.find(name="div", class_="col search_discount responsive_secondrow").text.replace("\n", "").strip()
+            )
+            gameinfo["原价"] = row.strike.string.strip().replace(" ", "")
+            gameinfo["折扣价"] = f'{str(discount_price).strip().replace(" ", "")}({discount_percent})'
         try:
-            rate = row.find(name = "span", class_ = "search_review_summary").get("data-tooltip-html")
-            gameinfo['评测'] = rate.replace("<br>", ",").replace(" ", "")
-        except:
-            gameinfo['评测'] = "暂无评测"
+            rate = row.find(name="span", class_="search_review_summary").get("data-tooltip-html")
+            gameinfo["评测"] = rate.replace("<br>", ",").replace(" ", "")
+        except Exception:
+            gameinfo["评测"] = "暂无评测"
         try:
             tag = row.get("data-ds-tagids").strip("[]").split(",")
-            tagk = ""
-            for i in tag:
-                tagk += tagdata["tag_dict"].get(i) + ","
-            gameinfo['标签'] = tagk.strip(",")
-        except:
-            gameinfo['标签'] = '无用户标签'
+            tagk = "".join(tagdata["tag_dict"].get(i) + "," for i in tag)
+            gameinfo["标签"] = tagk.strip(",")
+        except Exception:
+            gameinfo["标签"] = "无用户标签"
         result.append(gameinfo)
 
     return result
-# 根据传入的tag创建tag搜索链接,返回tag搜索链接以及传入的tag中有效的tag(有效tag具体参考data文件夹中的tag.json)
-def tagurl_creater(tag:list, page:int):
+
+
+# 根据传入的tag创建tag搜索链接，返回tag搜索链接以及传入的tag中有效的tag(有效tag具体参考data文件夹中的tag.json)
+def tagurl_creater(tag: list, page: int):
     tag_search_num = "&tags="
     tag_name = ""
     tag_list = tag
     count = f"&start={(page-1)*50}&count=50"
     for i in tag_list:
-        try:
+        if tagdata["tag_dict"].get(i, "") != "":
             tag_search_num += tagdata["tag_dict"][i] + ","
-            tag_name += i + ","
-        except:
-            pass
-    tag_search_url = "https://store.steampowered.com/search/results/?l=schinese&query&force_infinite=1&filter=topsellers&category1=998&infinite=1" + tag_search_num.strip(",") + count
-    return tag_search_url,tag_name.strip(",")
+            tag_name += f"{i},"
+    tag_search_url = (
+        "https://store.steampowered.com/search/results/?l=schinese&query&force_infinite=1&filter=topsellers&category1=998&infinite=1"
+        + tag_search_num.strip(",")
+        + count
+    )
+    return tag_search_url, tag_name.strip(",")
 
-def mes_creater(result:dict):
+
+def mes_creater(result: dict):
     mes_list = []
     for i in range(len(result)):
-        if result[i]['原价'] == "免费开玩" or result[i]['原价'] == "无价格信息":
+        if result[i]["原价"] in ["免费开玩", "无价格信息"]:
             mes = f"[CQ:image,file={result[i]['低分辨率图片']}]\n{result[i]['标题']}\n原价:{result[i]['原价']}\
                 \n链接:{result[i]['链接']}\n{result[i]['评测']}\n用户标签:{result[i]['标签']}\nappid:{result[i]['appid']}"
         else:
             mes = f"[CQ:image,file={result[i]['低分辨率图片']}]\n{result[i]['标题']}\n原价:{result[i]['原价']} 折扣价:{result[i]['折扣价']}\
                 \n链接:{result[i]['链接']}\n{result[i]['评测']}\n用户标签:{result[i]['标签']}\nappid:{result[i]['appid']}"
-        data = {
-        "type": "node",
-        "data": {
-            "name": "sbeam机器人",
-            "uin": "2854196310",
-            "content":mes
-                }
-            }
+        data = {"type": "node", "data": {"name": "sbeam机器人", "uin": "2854196310", "content": mes}}
         mes_list.append(data)
     return mes_list
 
+
 # 匹配关键词发送相关信息,例:今日特惠,发送今日特惠信息,今日新品则发送新品信息
-@sv.on_prefix('今日')
+@sv.on_prefix("今日")
 async def Gameinfo(bot, ev):
     model = ev.message.extract_plain_text().strip()
     try:
@@ -109,7 +115,7 @@ async def Gameinfo(bot, ev):
         else:
             return
     except Exception as e:
-        sv.logger.error(f"Error:{e}")
+        sv.logger.error(f"Error:{traceback.format_exc()}")
         await bot.send(ev, f"哦吼,出错了,报错内容为:{e},请检查运行日志!")
         return
     await bot.send(ev, "正在生成消息,请稍等片刻!", at_sender=True)
@@ -117,25 +123,25 @@ async def Gameinfo(bot, ev):
         if send_pic_mes:
             await bot.send(ev, f"[CQ:image,file={pic_creater(data, is_steam=True)}]")
             return
-        await bot.send_group_forward_msg(group_id=ev['group_id'], messages=mes_creater(data))
+        await bot.send_group_forward_msg(group_id=ev["group_id"], messages=mes_creater(data))
     except Exception as err:
         if "retcode=100" in str(err):
             await bot.send(ev, "消息可能被风控,正在转为其他形式发送!")
             try:
                 if send_pic_mes:
-                    await bot.send_group_forward_msg(group_id=ev['group_id'], messages=mes_creater(data))
+                    await bot.send_group_forward_msg(group_id=ev["group_id"], messages=mes_creater(data))
                     return
-                if not send_pic_mes:
-                    await bot.send(ev, f"[CQ:image,file={pic_creater(data, is_steam=True)}]")
+                await bot.send(ev, f"[CQ:image,file={pic_creater(data, is_steam=True)}]")
             except Exception as err:
                 if "retcode=100" in str(err):
                     await bot.send(ev, "消息可能依旧被风控,无法完成发送!")
         else:
-            sv.logger.error(f"Error:{err}")
+            sv.logger.error(f"Error:{traceback.format_exc()}")
             await bot.send(ev, f"发生了其他错误,报错内容为{err},请检查运行日志!")
 
+
 # 后接格式:页数(阿拉伯数字) 标签1 标签2,例:st搜标签 动作 射击
-@sv.on_prefix(('st搜','St搜','ST搜','sT搜'))
+@sv.on_prefix(("st搜", "St搜", "ST搜", "sT搜"))
 async def search_tag(bot, ev):
     mes = ev.message.extract_plain_text().strip()
     try:
@@ -148,13 +154,13 @@ async def search_tag(bot, ev):
             data = steam_crawler(tagurl[0])
         elif "游戏" in mes:
             gamename = mes[2:]
-            search_url = r"https://store.steampowered.com/search/results/?l=schinese&query&start=0&count=50&dynamic_data=&sort_by=_ASC&snr=1_7_7_151_7&infinite=1&term=" + gamename
+            search_url = f"https://store.steampowered.com/search/results/?l=schinese&query&start=0&count=50&dynamic_data=&sort_by=_ASC&snr=1_7_7_151_7&infinite=1&term={gamename}"
             data = steam_crawler(search_url)
             if len(data) == 0:
                 await bot.send(ev, "无搜索结果")
                 return
     except Exception as e:
-        sv.logger.error(f"Error:{e}")
+        sv.logger.error(f"Error:{traceback.format_exc()}")
         await bot.send(ev, f"哦吼,出错了,报错内容为{e},请检查运行日志!")
         return
     try:
@@ -168,27 +174,27 @@ async def search_tag(bot, ev):
         if send_pic_mes:
             await bot.send(ev, f"[CQ:image,file={pic_creater(data, is_steam=True)}]")
             return
-        await bot.send_group_forward_msg(group_id=ev['group_id'], messages=mes_creater(data))
+        await bot.send_group_forward_msg(group_id=ev["group_id"], messages=mes_creater(data))
     except Exception as err:
         if "retcode=100" in str(err):
             await bot.send(ev, "消息可能被风控,正在转为其他形式发送!")
             try:
                 if send_pic_mes:
-                    await bot.send_group_forward_msg(group_id=ev['group_id'], messages=mes_creater(data))
+                    await bot.send_group_forward_msg(group_id=ev["group_id"], messages=mes_creater(data))
                     return
-                if not send_pic_mes:
-                    await bot.send(ev, f"[CQ:image,file={pic_creater(data, is_steam=True)}]")
+                await bot.send(ev, f"[CQ:image,file={pic_creater(data, is_steam=True)}]")
             except Exception as err:
                 if "retcode=100" in str(err):
                     await bot.send(ev, "消息可能依旧被风控,无法完成发送!")
         else:
-            sv.logger.error(f"Error:{err}")
+            sv.logger.error(f"Error:{traceback.format_exc()}")
             await bot.send(ev, f"发生了其他错误,报错内容为{err},请检查运行日志!")
-            
-@sv.on_fullmatch(('st帮助','St帮助','ST帮助','sT帮助'))
+
+
+@sv.on_fullmatch(("st帮助", "St帮助", "ST帮助", "sT帮助"))
 async def help(bot, ev):
     helpimg = Image.open(os.path.join(FILE_PATH, "data/help.png"))
     b_io = io.BytesIO()
-    helpimg.save(b_io, format = "png")
-    base64_str = 'base64://' + base64.b64encode(b_io.getvalue()).decode()
-    await bot.send(ev, f'[CQ:image,file={base64_str}]')
+    helpimg.save(b_io, format="png")
+    base64_str = f"base64://{base64.b64encode(b_io.getvalue()).decode()}"
+    await bot.send(ev, f"[CQ:image,file={base64_str}]")
